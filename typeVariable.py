76060a7bf7ef4f13c6d1133a46f96e7850c68759
Saumy Sharma <saumysharma007@gmail.com>
2023-09-19 22:34:12 +0530

    type of data

---
+++ typeVariable.py
@@ -0,0 +1,6 @@
+a = 10 
+b = "Saumy"
+print("Value of DataType the a is ", a)
+print("Value of DataType the b is ", b)
+print("type of DataType the a is ", type(a))
+print("type of DataType the b is ", type(b))
