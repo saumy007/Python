3817caa02a4df9b38a09356e2ac9ba5ba38fb956
Saumy Sharma <saumysharma007@gmail.com>
2023-09-19 22:37:35 +0530

    more datatyppes

--- typeVariable.py
+++ typeVariable.py
@@ -1,6 +1,14 @@
 a = 10 
+d = complex(8,12)
 b = "Saumy"
+c = True
 print("Value of DataType the a is ", a)
 print("Value of DataType the b is ", b)
+print("Value of DataType the c is ", c)
+print("Value of DataType the d is ", d)
 print("type of DataType the a is ", type(a))
 print("type of DataType the b is ", type(b))
+print("type of DataType the c is ", type(c))
+print("type of DataType the d is ", type(d))
+
+
