eefe9364ad664d8ea33a0887dad19576730c99a6
saumy007 <saumysharma007@gmail.com>
2025-06-03 10:54:50 +0530

    classes

--- classes.py
+++ classes.py
@@ -1,16 +1,14 @@
 class Person:
-    name = "Saumy"
-    occupation = "developer"
-    networth = 10
+    def __init__(self,n,o):
+        print("hey this is saumy")
+        self.name = n
+        self.occ = o
     def info(self):
-        print(f"{self.name} is a {self.occupation}")
+        print(f"{self.name} is a {self.occ}")
 
 
-a = Person()
-b = Person()
-
-a.name = "Sam"
-a.occupation = "Person"
 
 
-print(a)
+
+a = Person("saumy","I am here")
+a.info()
